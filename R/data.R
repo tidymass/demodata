e6395f279d46b0cb8c09819e16a4c50990a88c1e
jaspershen <shenxt1990@outlook.com>
2021-12-20 20:36:40 -0800

    add liver dataset

--- R/data.R
+++ R/data.R
@@ -14,3 +14,22 @@
 #' @format mass_dataset object
 "object2"
 
+
+
+
+#' liver aging dataset pos from MetDNA
+#'
+#' A mass_dataset object
+#' A mass_dataset object
+#'
+#' @format mass_dataset object
+"liver_aging_pos"
+
+#' liver aging dataset neg from MetDNA
+#'
+#' A mass_dataset object
+#' A mass_dataset object
+#'
+#' @format mass_dataset object
+"liver_aging_neg"
+
